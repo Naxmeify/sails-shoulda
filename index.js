9838733d610d8fed69ff5735c7c5299a699973b5
Nax (aka Matt) <m.neundorf@live.de>
2015-05-18 12:36:38 +0000

    bla...

--- index.js
+++ index.js
@@ -9,8 +9,31 @@
  * should.validate...
  */
  
-var should = require('should');
-should.shoulda = require(require("path").join(__dirname, 'package.json'));
+//var should = require('should');
+//should.shoulda = require(require("path").join(__dirname, 'package.json'));
 
-require(require("path").join(__dirname, "lib/helper"));
+// Helper
+//require(require("path").join(__dirname, "lib/helper"));
 
+// Matcher
+//require(require("path").join(__dirname, "lib/matcher", "model_attribute_required"));
+//require(require("path").join(__dirname, "lib/matcher", "model_attribute_unique"));
+console.log("Hmm");
+var Assertion = require('should').Assertion;
+
+
+Assertion.add('ahelper', function(other, description) {
+    console.log("ahelper");
+    console.log(this);
+    console.log(other);
+    console.log(description);
+    
+    this.params = { operator: 'to be truthy' };
+
+    this.assert(this);
+});
+
+Assertion.alias('ahelper', 'LOL');
+
+
+// console.log(Object.keys(should));
--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,9 @@
-describe('NodeModule: Should', function() {
-  
+require('should-approximately-deep');
+require('../index');
+
+describe('NodeModule Shoulda', function() {
+  it('should', function() {
+    [1, 2, 3].should.not.be.approximatelyDeep([1, 2], 0.1);
+    true.should.be.LOL("asd");
+  });
 });
