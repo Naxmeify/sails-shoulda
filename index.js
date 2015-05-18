e9de5491362e0f9508a11a758e94a2817ff3d83a
Nax (aka Matt) <m.neundorf@live.de>
2015-05-18 11:23:58 +0000

    bootstrap lib struc

---
+++ index.js
@@ -0,0 +1,16 @@
+
+/**
+ * TODO:
+ * model matchers
+ * service matchers
+ * 
+ * Let's start as helper
+ * 
+ * should.validate...
+ */
+ 
+var should = require('should');
+should.shoulda = require(require("path").join(__dirname, 'package.json'));
+
+require(require("path").join(__dirname, "lib/helper"));
+
